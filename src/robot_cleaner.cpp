a84c4e7d448c2a6d4ec7b3bc5a368ae2690fbd23
Izzet Akkus <izzetakkus50@gmail.com>
2022-07-20 16:47:32 +0300

    v3

--- src/robot_cleaner.cpp
+++ src/robot_cleaner.cpp
@@ -11,6 +11,7 @@
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Pose.h>
 #include <cmath>
+#include <math.h>
 
 ros::Publisher velocity_publisher;
 ros::Subscriber pose_subscriber;
@@ -21,6 +22,7 @@ void rotate(double angular_speed,double angle, bool clockwise);
 double degree2radian(double angle_in_degree);
 void poseCallback(const turtlesim::PoseConstPtr & pose_message);
 void setDesiredOrientation(double desired_angle_radians);
+void moveGoal(turtlesim::Pose goal_pose, double distance_tolerance);
 
 
 double x{0};
@@ -47,6 +49,8 @@ int main(int argc, char  **argv )
     ros::spinOnce();
 
     velocity_publisher = node.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
+
+    ros::Rate loop_rate(10);
 
     /*
     cout << "Enter the speed: ";
@@ -76,8 +80,6 @@ int main(int argc, char  **argv )
 
     rotate(degree2radian(angular_speed), degree2radian(angle), clockwise);
     
-    */
-
 
     setDesiredOrientation(degree2radian(120));
     ros::Rate loop_rate(0.5);
@@ -86,6 +88,18 @@ int main(int argc, char  **argv )
     loop_rate.sleep();
     setDesiredOrientation(degree2radian(0));
 
+    */
+
+    turtlesim::Pose goal_pose;
+
+    goal_pose.x = 1;
+    goal_pose.y = 1;
+    goal_pose.theta = 0;
+
+    moveGoal(goal_pose,0.01);
+
+    loop_rate.sleep();
+    
     
     return 0;
 }
@@ -188,3 +202,38 @@ void setDesiredOrientation(double desired_angle_radians){
     rotate(abs(relative_angle_radians)/4, abs(relative_angle_radians), clockwise);
 
 }
+
+double getDistance(double act_x, double act_y, double des_x, double des_y){
+    return sqrt( pow((act_x - des_x),2) + pow((act_y - des_y),2) );
+}
+
+void moveGoal(turtlesim::Pose goal_pose, double distance_tolerance){
+
+    geometry_msgs::Twist vel_msg;
+    ros::Rate loop_rate(10);
+    double Kp{1.5};
+    double Ka{4};
+
+    do{
+        /**** Propotional Control ****/
+
+        //Linear velocity
+        vel_msg.linear.x = Kp*getDistance(turtlesim_pose.x, turtlesim_pose.y, goal_pose.x, goal_pose.y);
+
+        //Angular velocity
+
+        vel_msg.angular.x = 0;
+        vel_msg.angular.y = 0;
+        vel_msg.angular.z = Ka*(atan2(goal_pose.y - turtlesim_pose.y, goal_pose.x - turtlesim_pose.x));
+        velocity_publisher.publish(vel_msg);
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    } while(getDistance(turtlesim_pose.x, turtlesim_pose.y, goal_pose.x, goal_pose.y));
+
+    cout << "Reached the desired point!" << endl;
+    vel_msg.linear.x = 0;
+    vel_msg.angular.z = 0;
+    velocity_publisher.publish(vel_msg);
+}
+
